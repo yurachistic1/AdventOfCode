2c93fba84f8ab3282c3752b0094dcc9994099c87
Yury Chistov <yurachistic@gmail.com>
2019-12-16 16:42:33 +0000

    reworked day 7 completely

--- src/Day7.java
+++ src/Day7.java
@@ -1,180 +1,80 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Day7 {
 
-    public static void day7a() throws Exception {
-
-        IntCodeComputer intCodeComputer = new IntCodeComputer("./inputs/Day7.txt", 0);
-
-        long currentMax = 0;
-
-        for (int i = 0; i < 5; i++) {
-            intCodeComputer.setInput(0);
-            intCodeComputer.setPhaseSetting(i);
-            intCodeComputer.executeProgram();
-            intCodeComputer.refreshProgram();
-            long output1 = intCodeComputer.getOutput();
-            for (int j = 0; j < 5; j++) {
-                if (j == i) {
-                    continue;
-                }
-                intCodeComputer.setInput(output1);
-                intCodeComputer.setPhaseSetting(j);
-                intCodeComputer.executeProgram();
-                intCodeComputer.refreshProgram();
-                long output2 = intCodeComputer.getOutput();
-                for (int k = 0; k < 5; k++) {
-                    if (k == j || k == i) {
-                        continue;
-                    }
-                    intCodeComputer.setInput(output2);
-                    intCodeComputer.setPhaseSetting(k);
-                    intCodeComputer.executeProgram();
-                    intCodeComputer.refreshProgram();
-                    long output3 = intCodeComputer.getOutput();
-                    for (int l = 0; l < 5; l++) {
-                        if (l == i || l == j || l == k) {
-                            continue;
-                        }
-                        intCodeComputer.setInput(output3);
-                        intCodeComputer.setPhaseSetting(l);
-                        intCodeComputer.executeProgram();
-                        intCodeComputer.refreshProgram();
-                        long output4 = intCodeComputer.getOutput();
-                        for (int m = 0; m < 5; m++) {
-                            if (m == i || m == j || m == k || m == l) {
-                                continue;
-                            }
-                            intCodeComputer.setInput(output4);
-                            intCodeComputer.setPhaseSetting(m);
-                            intCodeComputer.executeProgram();
-                            intCodeComputer.refreshProgram();
-                            if (intCodeComputer.getOutput() > currentMax) {
-                                currentMax = intCodeComputer.getOutput();
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        System.out.println(currentMax);
+    public static void main(String[] args) {
+        day7(5, 5);
     }
 
-    public static void day7b() {
+    public static void day7(int noOfPhases, int noOfThrusters) {
 
-        IntCodeComputer thrusterA = new IntCodeComputer("./inputs/Day7.txt", 0);
-        IntCodeComputer thrusterB = new IntCodeComputer("./inputs/Day7.txt", 0);
-        IntCodeComputer thrusterC = new IntCodeComputer("./inputs/Day7.txt", 0);
-        IntCodeComputer thrusterD = new IntCodeComputer("./inputs/Day7.txt", 0);
-        IntCodeComputer thrusterE = new IntCodeComputer("./inputs/Day7.txt", 0);
-//
-//        thrusterA.setPhaseSetting(9);
-//        thrusterB.setPhaseSetting(8);
-//        thrusterC.setPhaseSetting(7);
-//        thrusterD.setPhaseSetting(6);
-//        thrusterE.setPhaseSetting(5);
+        ArrayList<int[]> permutations = generatePermutations(noOfPhases);
 
-        thrusterA.setInterruptAfterOutput(true);
-        thrusterB.setInterruptAfterOutput(true);
-        thrusterC.setInterruptAfterOutput(true);
-        thrusterD.setInterruptAfterOutput(true);
-        thrusterE.setInterruptAfterOutput(true);
+        ArrayList<IntCodeComputer> thrusters = (ArrayList<IntCodeComputer>) IntStream
+                .range(0, noOfThrusters)
+                .mapToObj(i -> new IntCodeComputer("./inputs/Day7.txt", 0))
+                .collect(Collectors.toList());
 
         long max = 0;
-//
-//        while(!thrusterE.isTerminated()){
-//            thrusterA.setInput(outputE);
-//            thrusterA.executeProgram();
-//            outputA = thrusterA.getOutput();
-//
-//            thrusterB.setInput(outputA);
-//            thrusterB.executeProgram();
-//            outputB = thrusterB.getOutput();
-//
-//            thrusterC.setInput(outputB);
-//            thrusterC.executeProgram();
-//            outputC = thrusterC.getOutput();
-//
-//            thrusterD.setInput(outputC);
-//            thrusterD.executeProgram();
-//            outputD = thrusterD.getOutput();
-//
-//            thrusterE.setInput(outputD);
-//            thrusterE.executeProgram();
-//            outputE = thrusterE.getOutput();
-//            System.out.println(outputE);
-//        }
-//
-//        System.out.println(outputE);
-//
 
-        for (int i = 5; i < 10; i++) {
-            for (int j = 5; j < 10; j++) {
-                if (j == i) {
-                    continue;
-                }
-                for (int k = 5; k < 10; k++) {
-                    if (k == j || k == i) {
-                        continue;
-                    }
-                    for (int l = 5; l < 10; l++) {
-                        if (l == i || l == j || l == k) {
-                            continue;
-                        }
-                        for (int m = 5; m < 10; m++) {
-                            if (m == i || m == j || m == k || m == l) {
-                                continue;
-                            }
+        for (int[] permutation : permutations) {
+            long output = 0;
+            thrusters.forEach(IntCodeComputer::refreshProgram);
+            for (int i = 0; i < permutation.length; i++) {
+                thrusters.get(i).setInput(output);
+                thrusters.get(i).setPhaseSetting(permutation[i]);
+                thrusters.get(i).executeProgram();
+                output = thrusters.get(i).getOutput();
+            }
+            max = Math.max(output, max);
+        }
 
-                            //System.out.println(String.format("%d %d %d %d %d", i, j, k, l, m));
-                            thrusterA.refreshProgram();
-                            thrusterB.refreshProgram();
-                            thrusterC.refreshProgram();
-                            thrusterD.refreshProgram();
-                            thrusterE.refreshProgram();
+        thrusters.forEach(intCodeComputer -> intCodeComputer.setExecutingLazily(true));
+        long maxWithFeedbackLoop = 0;
 
-
-                            thrusterA.setPhaseSetting(i);
-                            thrusterB.setPhaseSetting(j);
-                            thrusterC.setPhaseSetting(k);
-                            thrusterD.setPhaseSetting(l);
-                            thrusterE.setPhaseSetting(m);
-
-                            long outputA = 0;
-                            long outputB = 0;
-                            long outputC = 0;
-                            long outputD = 0;
-                            long outputE = 0;
-
-                            while(!thrusterE.isTerminated()) {
-
-                                thrusterA.setInput(outputE);
-                                thrusterA.executeProgram();
-                                outputA = thrusterA.getOutput();
-
-                                thrusterB.setInput(outputA);
-                                thrusterB.executeProgram();
-                                outputB = thrusterB.getOutput();
-
-                                thrusterC.setInput(outputB);
-                                thrusterC.executeProgram();
-                                outputC = thrusterC.getOutput();
-
-                                thrusterD.setInput(outputC);
-                                thrusterD.executeProgram();
-                                outputD = thrusterD.getOutput();
-
-                                thrusterE.setInput(outputD);
-                                thrusterE.executeProgram();
-                                outputE = thrusterE.getOutput();
-                            }
-                            //System.out.println(outputE);
-                            if(outputE > max){
-                                max = outputE;
-                            }
-                        }
-                    }
+        for (int[] permutation : permutations) {
+            long output = 0;
+            thrusters.forEach(IntCodeComputer::refreshProgram);
+            IntStream.range(0,5).forEach(i -> thrusters.get(i).setPhaseSetting(permutation[i] + 5)); //instead of generating new set of permutations just add five to each element
+            while (!thrusters.get(thrusters.size() - 1).isTerminated()) {
+                for (int i = 0; i < thrusters.size(); i++) {
+                    thrusters.get(i).setInput(output);
+                    output = thrusters.get(i).getOutput();
                 }
             }
+            maxWithFeedbackLoop = Math.max(output, maxWithFeedbackLoop);
         }
-        System.out.println(max);
+
+        System.out.printf("Part one: %d\n", max);
+        System.out.printf("Part two: %d\n", maxWithFeedbackLoop);
+    }
+
+    public static ArrayList<int[]> generatePermutations(int noOfPhases) {
+        ArrayList<int[]> permutations = new ArrayList<>();
+        int[] members = IntStream.range(0, noOfPhases).toArray();
+        heapPermutation(permutations, members, noOfPhases);
+        return permutations;
+    }
+
+    public static void heapPermutation(ArrayList<int[]> permutations, int a[], int size) {
+        if (size == 1)
+            permutations.add(Arrays.copyOf(a, a.length));
+
+        for (int i = 0; i < size; i++) {
+            heapPermutation(permutations, a, size - 1);
+
+            if (size % 2 == 1) {
+                int temp = a[0];
+                a[0] = a[size - 1];
+                a[size - 1] = temp;
+            } else {
+                int temp = a[i];
+                a[i] = a[size - 1];
+                a[size - 1] = temp;
+            }
+        }
     }
 }
