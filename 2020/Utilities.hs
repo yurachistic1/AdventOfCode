565cdbee77972cc00719661e5bfa1ea3e49aea46
Yury Chistov <yurachistic@gmail.com>
2020-12-04 14:41:47 +0000

    Add splitting utilities

--- 2020/Utilities.hs
+++ 2020/Utilities.hs
@@ -12,6 +12,25 @@ frequencyMap = M.unionsWith (+) . map (flip M.singleton 1)
 indexMap :: [a] -> M.Map Int a
 indexMap = M.fromList . zip [0 ..]
 
-tails :: [a] -> [(a, [a])]
-tails [x] = [(x, [])]
-tails (x : xs) = (x, xs) : tails xs
+keyValue :: Eq a => a -> [a] -> Maybe ([a], [a])
+keyValue val list
+  | (k, _ : v) <- kv = Just (k, v)
+  | otherwise = Nothing
+  where
+    kv = break (== val) list
+
+-- From Data.List.SplitOn --
+-- Copied to just better understand how it works
+
+splitOn :: (a -> Bool) -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn f l@(x : xs)
+  | f x = splitOn f xs
+  | otherwise = let (h, t) = break f l in h : (splitOn f t)
+
+splitOn' :: (a -> Bool) -> [a] -> [[a]]
+splitOn' f xs = split xs
+  where
+    split xs = case break f xs of
+      (chunk, []) -> chunk : []
+      (chunk, _ : rest) -> chunk : split rest
