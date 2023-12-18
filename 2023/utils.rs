bf73679e04e50b1544ac06dd5c562430536de609
Yury Chistov <yurachistic@gmail.com>
2023-12-18 10:40:20 +0000

    add get_adjacent_8

--- 2023/utils.rs
+++ 2023/utils.rs
@@ -15,3 +15,26 @@ pub fn read_input(file_path: &str) -> Vec<String> {
         .filter(|line| !line.is_empty())
         .collect()
 }
+
+pub fn get_adjacent_8(x: usize, y: usize, x_bound: usize, y_bound: usize) -> Vec<(usize, usize)> {
+    let mut res: Vec<(usize, usize)> = vec![(x + 1, y), (x + 1, y + 1), (x, y + 1)];
+
+    if x > 0 {
+        res.push((x - 1, y));
+        res.push((x - 1, y + 1));
+    }
+
+    if y > 0 {
+        res.push((x, y - 1));
+        res.push((x + 1, y - 1));
+    }
+
+    if x > 0 && y > 0 {
+        res.push((x - 1, y - 1))
+    }
+
+    res.iter()
+        .filter(|(a, b)| a < &x_bound && b < &y_bound)
+        .cloned()
+        .collect()
+}
